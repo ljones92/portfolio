3d2cbf6b26d69aeff03f0fffcdf5e2cdb84e4898
Laurence Jones <laurence.jones@sky.uk>
2017-07-23 01:27:44 +0100

    Fixed height of particle canvas on larger screens

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.1.1]
+### Fixed
+- Fixed height of particle canvas on larger screens to help prevent performance issues
+
 ## [1.1.0]
 ### Added
 - Updated theming of site
--- app/views/Title.jsx
+++ app/views/Title.jsx
@@ -4,7 +4,7 @@ import ParticleCanvas from '../components/ParticleCanvas';
 
 const Title = () => (
     <div className='title'>
-        <ParticleCanvas height={window.innerWidth < 1200 ? '600px' : '100%'} />
+        <ParticleCanvas height={window.innerWidth < 1200 ? '600px' : '800px'} />
         <div className='title__heading'>
             <h1>Laurence Jones</h1>
             <span>Software Engineer</span>
