662a0a603727e4460a4962afa4672273c609aea5
Laurence Jones <laurence.jones@sky.uk>
2017-07-23 01:38:50 +0100

    Fixed test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.1.2]
+### Fixed
+- Fixed test
+
 ## [1.1.1]
 ### Fixed
 - Fixed height of particle canvas on larger screens to help prevent performance issues
--- test/views/Title.test.js
+++ test/views/Title.test.js
@@ -17,9 +17,9 @@ describe('View: Title', () => {
     expect(wrapper.find('ParticleCanvas').node.props.height).to.equal('600px');
   });
 
-  it('Contains a particle canvas of height 100% if window width is greater than 1200px', () => {
+  it('Contains a particle canvas of height 800px if window width is greater than 1200px', () => {
     global.window = { innerWidth: 1300 };
     const wrapper = shallow(<Title />);
-    expect(wrapper.find('ParticleCanvas').node.props.height).to.equal('100%');
+    expect(wrapper.find('ParticleCanvas').node.props.height).to.equal('800px');
   });
 });
