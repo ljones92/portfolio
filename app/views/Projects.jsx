85c139cbf7ea214bdd309c386dcb0d130fef0c67
Laurence Jones <laurence.jones@infinityworks.com>
2017-12-19 00:05:17 +0000

    Updated job info and added keybase file

--- app/views/Projects.jsx
+++ app/views/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ScrollableAnchor from 'react-scrollable-anchor';
 
+const super6 = require('../assets/img/super6-logo.png');
+const pick7 = require('../assets/img/pick7-logo.png');
 const news = require('../assets/img/sky-news.png');
 const sports = require('../assets/img/sky-sports.svg');
 
@@ -10,13 +12,23 @@ const Projects = () => (
             <h3 className='panel__title'>Projects</h3>
         </ScrollableAnchor>
         <p className='panel__body'>
-            I currently work on the team responsible for developing Sky News and Sky Sports.
-            Some of the projects I have been involved with include implementing AMP on both
-            sites and working on the news elections site during the General Election.
+            I currently work as part of a consultantany team contracted out to SkyBet
+            to work on various free to play games including Super 6 and Pick 7. In the past
+            I have also worked on high profile projects such as Sky News and Sky Sports.
         </p>
         <div className='panel__columns'>
             <div className='panel__image'>
-                <a href='http://news.sky.com'>
+                <a href='https://super6.skysports.com'>
+                    <img src={super6} alt='Super 6' />
+                </a>
+            </div>
+            <div className='panel__image'>
+                <a href='https://pick7.sportinglife.com'>
+                    <img src={pick7} alt='Pick 7' />
+                </a>
+            </div>
+            <div className='panel__image'>
+                <a href='https://news.sky.com'>
                     <img src={news} alt='Sky News' />
                 </a>
             </div>
